74d3846cd2943970e7dea49032d810c37c062c2d
yan <115050813@qq.com>
2017-11-23 19:27:42 +0800

    实现了四元数的运算。

--- src/main/java/net/jmecn/math/Quaternion.java
+++ src/main/java/net/jmecn/math/Quaternion.java
@@ -10,6 +10,11 @@ public class Quaternion {
 
     public float x, y, z, w;
 
+    // 零四元数
+    public final static Quaternion ZERO = new Quaternion(0, 0, 0, 0);
+    // 单位四元数
+    public final static Quaternion IDENTITY = new Quaternion(0, 0, 0, 1);
+    
     public Quaternion() {
         x = y = z = 0;
         w = 1;
@@ -30,54 +35,120 @@ public class Quaternion {
     }
 
     /**
-     * 使用绕轴旋转角度来初始化四元数
+     * 使用角轴对来计算四元数
      * 
-     * @param axis
-     * @param angle
+     * @param axis 由单位向量定义的旋转轴
+     * @param angle 旋转角（弧度制）
      */
     public Quaternion(Vector3f axis, float angle) {
-        axis = axis.normalize();
+        axis.normalizeLocal();
         
-        float sinHalfAngle = (float) Math.sin(angle / 2);
-        float cosHalfAngle = (float) Math.cos(angle / 2);
-
-        this.x = axis.x * sinHalfAngle;
-        this.y = axis.y * sinHalfAngle;
-        this.z = axis.z * sinHalfAngle;
-        this.w = cosHalfAngle;
+        float cosHalfAngle = (float) Math.cos(angle * 0.5);
+        float sinHalfAngle = (float) Math.sin(angle * 0.5);
+        
+        w = cosHalfAngle;
+        x = axis.x * sinHalfAngle;
+        y = axis.y * sinHalfAngle;
+        z = axis.z * sinHalfAngle;
     }
 
     /**
-     * 设置四元数的四个分量
-     * 
-     * @param x
-     * @param y
-     * @param z
-     * @param w
+     * 使用角轴对来计算四元数
+     * @param axis 由单位向量定义的旋转轴
+     * @param angle 旋转角（弧度制）
      * @return
      */
-    public Quaternion set(float x, float y, float z, float w) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.w = w;
+    public Quaternion fromAxisAngle(Vector3f axis, float angle) {
+        axis.normalizeLocal();
+        
+        float sinHalfAngle = (float) Math.sin(angle * 0.5);
+        float cosHalfAngle = (float) Math.cos(angle * 0.5);
+
+        w = cosHalfAngle;
+        
+        x = axis.x * sinHalfAngle;
+        y = axis.y * sinHalfAngle;
+        z = axis.z * sinHalfAngle;
+        
+        return this;
+    }
+    
+    /**
+     * 绕X轴旋转
+     * @param angle
+     * @return
+     */
+    public Quaternion rotateX(float angle) {
+        float sinHalfAngle = (float) Math.sin(angle * 0.5);
+        float cosHalfAngle = (float) Math.cos(angle * 0.5);
+
+        w = cosHalfAngle;
+        
+        x = sinHalfAngle;
+        y = 0;
+        z = 0;
+        
         return this;
     }
 
     /**
-     * 设置四元数的四个分量
-     * 
-     * @param q
+     * 绕Y轴旋转
+     * @param angle
      * @return
      */
-    public Quaternion set(Quaternion q) {
-        this.x = q.x;
-        this.y = q.y;
-        this.z = q.z;
-        this.w = q.w;
+    public Quaternion rotateY(float angle) {
+        float sinHalfAngle = (float) Math.sin(angle * 0.5);
+        float cosHalfAngle = (float) Math.cos(angle * 0.5);
+
+        w = cosHalfAngle;
+        
+        x = 0;
+        y = sinHalfAngle;
+        z = 0;
+        
         return this;
     }
 
+    /**
+     * 绕Z轴旋转
+     * @param angle
+     * @return
+     */
+    public Quaternion rotateZ(float angle) {
+        float sinHalfAngle = (float) Math.sin(angle * 0.5);
+        float cosHalfAngle = (float) Math.cos(angle * 0.5);
+
+        w = cosHalfAngle;
+        
+        x = 0;
+        y = 0;
+        z = sinHalfAngle;
+        
+        return this;
+    }
+
+    /**
+     * 负四元数
+     * 
+     * @return
+     */
+    public Quaternion negate() {
+        return new Quaternion(-x, -y, -z, -w);
+    }
+
+    /**
+     * 负四元数
+     * 
+     * @return
+     */
+    public Quaternion negateLocal() {
+        x = -x;
+        y = -y;
+        z = -z;
+        w = -w;
+        return this;
+    }
+    
     /**
      * 将四元数设为 (0, 0, 0, 1)
      */
@@ -87,7 +158,7 @@ public class Quaternion {
     }
 
     /**
-     * 判断四元数的值是否为(0, 0, 0, 1)
+     * 判断是否为单位四元数
      * 
      * @return true
      */
@@ -98,41 +169,7 @@ public class Quaternion {
             return false;
         }
     }
-
-    /**
-     * 求负四元数
-     * 
-     * @return
-     */
-    public Quaternion negate() {
-        return new Quaternion(-x, -y, -z, -w);
-    }
-
-    /**
-     * 求共轭四元数
-     * 
-     * @return
-     */
-    public Quaternion conjugate() {
-        return new Quaternion(-x, -y, -z, w);
-    }
-
-    /**
-     * 求四元数的逆。 四元数的逆 = 四元数的共轭 / 四元数的模
-     * 
-     * @return
-     */
-    public Quaternion inverse() {
-        float length = lengthSquared();
-        if (length == 0f) {
-            return null;
-        } else if (length != 1f) {
-            length = (float) (1.0 / Math.sqrt(length));
-            return new Quaternion(-x * length, -y * length, -z * length, w * length);
-        }
-        // 单位四元数的逆和共轭是相等的。
-        return conjugate();
-    }
+    
 
     /**
      * 求长度的平方
@@ -149,34 +186,94 @@ public class Quaternion {
      * @return
      */
     public float length() {
-        return (float) Math.sqrt(lengthSquared());
+        return (float) Math.sqrt(x * x + y * y + z * z + w * w);
     }
 
     /**
-     * 求单位四元数
+     * 规范化四元数
      * 
      * @return
      */
     public Quaternion normalize() {
-        float length = lengthSquared();
+        float length = x * x + y * y + z * z + w * w;
         if (length != 1f && length != 0f) {
             length = (float) (1.0 / Math.sqrt(length));
-            return mult(length);
+            return new Quaternion(length * x, length * y, length * z, length * w);
         }
         return new Quaternion(x, y, z, w);
     }
 
-
     /**
-     * 标量乘法
+     * 规范化四元数
      * 
-     * @param scalar
      * @return
      */
-    public Quaternion mult(float scalar) {
-        return new Quaternion(scalar * x, scalar * y, scalar * z, scalar * w);
+    public Quaternion normalizeLocal() {
+        float length = x * x + y * y + z * z + w * w;
+        if (length != 1f && length != 0f) {
+            length = (float) (1.0 / Math.sqrt(length));
+            x *= length;
+            y *= length;
+            z *= length;
+            w *= length;
+        }
+        return this;
+    }
+    
+    /**
+     * 求共轭四元数
+     * 
+     * @return
+     */
+    public Quaternion conjugate() {
+        return new Quaternion(-x, -y, -z, w);
     }
 
+    /**
+     * 求共轭四元数
+     * 
+     * @return
+     */
+    public Quaternion conjugateLocal() {
+        x = -x;
+        y = -y;
+        z = -z;
+        return this;
+    }
+    
+    /**
+     * 求四元数的逆。 四元数的逆 = 四元数的共轭 / 四元数的模
+     * 
+     * @return
+     */
+    public Quaternion inverse() {
+        float length = x * x + y * y + z * z + w * w;
+        if (length != 1f && length != 0f) {
+            length = (float) (1.0 / Math.sqrt(length));
+            return new Quaternion(-x * length, -y * length, -z * length, w * length);
+        }
+        return new Quaternion(-x, -y, -z, w);
+    }
+    
+    /**
+     * 求四元数的逆。 四元数的逆 = 四元数的共轭 / 四元数的模
+     * 
+     * @return
+     */
+    public Quaternion inverseLocal() {
+        float length = x * x + y * y + z * z + w * w;
+        if (length != 1f && length != 0f) {
+            length = (float) (1.0 / Math.sqrt(length));
+            x *= length;
+            y *= length;
+            z *= length;
+        }
+        x = -x;
+        y = -y;
+        z = -z;
+        return this;
+    }
+    
     /**
      * 四元数乘法，或者叫叉乘。
      * 
@@ -185,11 +282,55 @@ public class Quaternion {
      */
     public Quaternion mult(Quaternion q) {
         float qw = q.w, qx = q.x, qy = q.y, qz = q.z;
+        
         float rw = w * qw - x * qx - y * qy - z * qz;
         float rx = w * qx + x * qw + y * qz - z * qy;
         float ry = w * qy + y * qw + z * qx - x * qz;
         float rz = w * qz + z * qw + x * qy - y * qx;
+        
         return new Quaternion(rx, ry, rz, rw);
+    }
+    
+    /**
+     * 四元数乘法，或者叫叉乘。
+     * 
+     * @param q
+     * @return
+     */
+    public Quaternion multLocal(Quaternion q) {
+        float qw = q.w, qx = q.x, qy = q.y, qz = q.z;
+        
+        float rw = w * qw - x * qx - y * qy - z * qz;
+        float rx = w * qx + x * qw + y * qz - z * qy;
+        float ry = w * qy + y * qw + z * qx - x * qz;
+        float rz = w * qz + z * qw + x * qy - y * qx;
+        
+        x = rx;
+        y = ry;
+        z = rz;
+        w = rw;
+        return this;
+    }
+    
+    /**
+     * 使用这个四元数来旋转一个三维向量。
+     * 
+     * @param v
+     * @return
+     */
+    public Vector3f mult(Vector3f v) {
+        if (v.x == 0 && v.y == 0 && v.z == 0) {
+            return new Vector3f(0, 0, 0);
+        } else {
+            float vx = v.x, vy = v.y, vz = v.z;
+            float rx = w * w * vx + 2 * y * w * vz - 2 * z * w * vy + x * x * vx + 2 * y * x * vy + 2 * z * x * vz
+                    - z * z * vx - y * y * vx;
+            float ry = 2 * x * y * vx + y * y * vy + 2 * z * y * vz + 2 * w * z * vx - z * z * vy + w * w * vy
+                    - 2 * x * w * vz - x * x * vy;
+            float rz = 2 * x * z * vx + 2 * y * z * vy + z * z * vz - 2 * w * y * vx - y * y * vz + 2 * w * x * vy
+                    - x * x * vz + w * w * vz;
+            return new Vector3f(rx, ry, rz);
+        }
     }
     
     /**
@@ -237,6 +378,57 @@ public class Quaternion {
     /**
      * 球面线性插值(Spherical Linear intERPolation)
      *
+     * @param src
+     * @param dest
+     * @param t
+     */
+    public Quaternion slerp(Quaternion src, Quaternion dest, float t) {
+        if (src.x == dest.x && src.y == dest.y && src.z == dest.z && src.w == dest.w) {
+            return new Quaternion(src);
+        }
+
+        // 用点乘计算两四元数夹角的 cos 值
+        float cos = src.dot(dest);
+
+        // 如果点乘为负，则反转一个四元数以取得短的4D“弧”
+        if (cos < 0.0f) {
+            cos = -cos;
+            dest = dest.negate();
+        }
+
+        // 计算两个四元数的插值系数
+        float srcFactor = 1 - t;
+        float destFactor = t;
+
+        // 检查它们是否过于接近以避免除零
+        if (cos > 0.999f) {
+            // 非常接近 -- 即线性插值
+            srcFactor = 1 - t;
+            destFactor = t;
+        } else {
+            // 计算两个四元数之间的夹角
+            float angle = (float) Math.acos(cos);
+            // 计算分母的倒数，这样就只需要一次除法
+            float invSin = 1f / (float) Math.sin(angle);
+
+            // 计算插值系数
+            srcFactor = (float) Math.sin((1 - t) * angle) * invSin;
+            destFactor = (float) Math.sin((t * angle)) * invSin;
+        }
+        
+        // 插值
+        float rx = (srcFactor * src.x) + (destFactor * dest.x);
+        float ry = (srcFactor * src.y) + (destFactor * dest.y);
+        float rz = (srcFactor * src.z) + (destFactor * dest.z);
+        float rw = (srcFactor * src.w) + (destFactor * dest.w);
+
+        // 返回插值结果
+        return new Quaternion(rx, ry, rz, rw);
+    }
+    
+    /**
+     * 球面线性插值(Spherical Linear intERPolation)
+     *
      * @param dest
      * @param t
      */
@@ -274,9 +466,6 @@ public class Quaternion {
             destFactor = (float) Math.sin((t * angle)) * invSin;
         }
         
-        // 注释掉这一句，避免多次实例化对象。下面的实现效果是一样的。
-        // return this.mult(srcFactor).add(dest.mult(destFactor));
-
         // 插值
         float rx = (srcFactor * x) + (destFactor * dest.x);
         float ry = (srcFactor * y) + (destFactor * dest.y);
@@ -287,25 +476,34 @@ public class Quaternion {
         return new Quaternion(rx, ry, rz, rw);
     }
     
-    
     /**
-     * 使用这个四元数来旋转一个三维向量。
+     * 设置四元数的四个分量
      * 
-     * @param v
+     * @param x
+     * @param y
+     * @param z
+     * @param w
      * @return
      */
-    public Vector3f mult(Vector3f v) {
-        if (v.x == 0 && v.y == 0 && v.z == 0) {
-            return new Vector3f(0, 0, 0);
-        } else {
-            float vx = v.x, vy = v.y, vz = v.z;
-            float rx = w * w * vx + 2 * y * w * vz - 2 * z * w * vy + x * x * vx + 2 * y * x * vy + 2 * z * x * vz
-                    - z * z * vx - y * y * vx;
-            float ry = 2 * x * y * vx + y * y * vy + 2 * z * y * vz + 2 * w * z * vx - z * z * vy + w * w * vy
-                    - 2 * x * w * vz - x * x * vy;
-            float rz = 2 * x * z * vx + 2 * y * z * vy + z * z * vz - 2 * w * y * vx - y * y * vz + 2 * w * x * vy
-                    - x * x * vz + w * w * vz;
-            return new Vector3f(rx, ry, rz);
-        }
+    public Quaternion set(float x, float y, float z, float w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+        return this;
+    }
+
+    /**
+     * 设置四元数的四个分量
+     * 
+     * @param q
+     * @return
+     */
+    public Quaternion set(Quaternion q) {
+        this.x = q.x;
+        this.y = q.y;
+        this.z = q.z;
+        this.w = q.w;
+        return this;
     }
 }
