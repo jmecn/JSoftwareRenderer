3a11e83725c3711844e24d987823fa08b23e7c50
yan <115050813@qq.com>
2017-11-28 15:10:26 +0800

    编写了一个测试用例，测试纹理采样。

--- src/test/java/net/jmecn/examples/Test3DTexture.java
+++ src/test/java/net/jmecn/examples/Test3DTexture.java
@@ -4,30 +4,36 @@ import net.jmecn.Application;
 import net.jmecn.math.Vector3f;
 import net.jmecn.scene.Box;
 import net.jmecn.scene.Mesh;
+import net.jmecn.scene.Texture;
 
 /**
- * 测试3D光栅化
+ * 测试纹理采样效果
  * @author yanmaoyuan
  *
  */
-public class Test3DRasterization extends Application {
+public class Test3DTexture extends Application {
 
     public static void main(String[] args) {
-        Test3DRasterization app = new Test3DRasterization();
+        Test3DTexture app = new Test3DTexture();
         app.setResolution(800, 600);
-        app.setTitle("3D Rasterization");
-        app.setFrameRate(60);
+        app.setTitle("Test Texture");
         app.start();
     }
-    
+
     @Override
     protected void initialize() {
         // 初始化摄像机
         getCamera().lookAt(new Vector3f(3, 4, 5),
                 Vector3f.ZERO, Vector3f.UNIT_Y);
         
-        // 添加到场景中
+        // 使用程序纹理
+        Texture texture = new Texture();
+        texture.setLinearFilter(true);
+        
         Mesh mesh = new Box();
+        mesh.setTexture(texture);
+        
+        // 添加到场景中
         meshes.add(mesh);
     }
 
